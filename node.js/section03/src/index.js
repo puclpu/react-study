ac2a4e172501fc537fc47f8c6827a2bf49dd79bc
kimchaerin <puclpu18@gmail.com>
2025-01-16 20:34:05 +0900

    3.5 Node.js 라이브러리

--- node.js/section03/src/index.js
+++ node.js/section03/src/index.js
@@ -1,5 +1,10 @@
 import mul, {add, sub} from "./math.js";
 
-console.log(add(1, 2));
-console.log(sub(1, 2));
-console.log(mul(1, 2));
+import randomcolor from "randomcolor";
+
+const color = randomcolor();
+console.log(color); // #5431c6 같은 색상 코드 출력
+
+// console.log(add(1, 2));
+// console.log(sub(1, 2));
+// console.log(mul(1, 2));
