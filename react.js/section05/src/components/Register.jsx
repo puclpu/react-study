e5fe49e1d976927148fa48353bc865ec0cd07815
kimchaerin <puclpu18@gmail.com>
2025-01-18 21:18:46 +0900

    5.10 useRef로 컴포넌트 변수 생성

--- react.js/section05/src/components/Register.jsx
+++ react.js/section05/src/components/Register.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 // 간단한 회원가입 폼
 // 1. 이름
 // 2. 생년월일
@@ -13,19 +13,29 @@ const Register = () => {
         bio: "",
     });
 
-    console.log(input);
+    const countRef = useRef(0);
+    const inputRef = useRef();
 
     const onChange = (e) => {
+        countRef.current++;
+        console.log(countRef.current);
         setInput({
             ...input,
             [e.target.name]: e.target.value,
         });
     };
 
+    const onSubmit = () => {
+        if (input.name === "") {
+            // 이름을 입력하는 DOM 요소 포커스
+            inputRef.current.focus();
+        }
+    };
+
     return (
         <div>
             <div>
-                <input name="name" value={input.name} onChange={onChange} placeholder={"이름"} />
+                <input ref={inputRef} name="name" value={input.name} onChange={onChange} placeholder={"이름"} />
             </div>
             <div>
                 <input type="date" name="birth" value={input.birth} onChange={onChange}/>
@@ -40,6 +50,7 @@ const Register = () => {
             <div>
                 <textarea name="bio" value={input.bio} onChange={onChange}/>
             </div>
+            <button onClick={onSubmit}>제출</button>
         </div>
     );
 }
