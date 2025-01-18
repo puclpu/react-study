ae28c63e95d669ac02f345057b76e4c2c6329566
kimchaerin <puclpu18@gmail.com>
2025-01-18 11:31:56 +0900

    5.2 React 컴포넌트

--- react.js/section05/src/App.jsx
+++ react.js/section05/src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
 
-function App() {
-  
-
+function App() { // 부모 컴퍼넌트
   return (
     <>
-      <h1>안녕 리액트!</h1>
+      {/* App 컴퍼넌트를 랜더링할 때, Header 컴퍼넌트도 함께 렌더링 됨 */}
+      <Header />
+      <Main />
+      <Footer />
     </>
   )
 }
---
+++ react.js/section05/src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() { // 자식 컴퍼넌트
+    return (
+      <header>
+        <h1>header</h1>
+      </header>
+    )
+};
+
+export default Header;
