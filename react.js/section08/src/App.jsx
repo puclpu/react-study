b37482ea5b64894adaff375083e9dcce468071ac
kimchaerin <puclpu18@gmail.com>
2025-01-19 12:21:02 +0900

    8.3 To Do List Mock Data 설정

--- react.js/section08/src/App.jsx
+++ react.js/section08/src/App.jsx
@@ -2,8 +2,31 @@ import './App.css'
 import Header from './components/Header'
 import Editor from './components/Editor'
 import List from './components/List'
+import { useState } from 'react'
+
+const mockData = [
+  {
+    id: 0,
+    isDone: false,
+    content: "React 공부하기", 
+    date: new Date().getTime(),
+  },
+  {
+    id: 1,
+    isDone: false,
+    content: "JavaScript 공부하기", 
+    date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    isDone: false,
+    content: "Java 공부하기", 
+    date: new Date().getTime(),
+  }
+];
 
 function App() {
+  const [todos, setTodos] = useState(mockData);
 
   return (
     <div className='App'>
