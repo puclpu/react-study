7c9c68ca404d20492e719cde6b28f36a0df8d21d
kimchaerin <puclpu18@gmail.com>
2025-01-22 23:04:47 +0900

    12.7 Emotion Diary 공통 컴포넌트 구현

--- react.js/section12/src/App.jsx
+++ react.js/section12/src/App.jsx
@@ -5,6 +5,8 @@ import Diary from './pages/Diary'
 import New from './pages/New'
 import Notfound from './pages/Notfount'
 import { getEmotionImage } from './util/get-emotion-image'
+import Button from './components/Button'
+import Header from './components/Header'
 
 // 1. "/" : 모은 일기를 조회하는 Home 페이지
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
@@ -19,19 +21,17 @@ const onClickButton = () => {
 
   return (
     <>
-      <div>
-        <img src={getEmotionImage(1)} />
-        <img src={getEmotionImage(2)} />
-        <img src={getEmotionImage(3)} />
-        <img src={getEmotionImage(4)} />
-        <img src={getEmotionImage(5)} />
-      </div>
-      <div>
-        <Link to={"/"}>Home</Link>
-        <Link to={"/new"}>New</Link>
-        <Link to={"/diary"}>Diary</Link>
-      </div>
-      <button onClick={onClickButton}>New 페이지로 이동</button>
+      <Header title={"Header"} leftChild={<Button text={"Left"}/>} rightChild={<Button text={"Right"}/>}/>
+      <Button text={"123"} type={"DEFAULT"} onClick={()=>{
+        console.log("123번 버튼 클릭");
+      }}/>
+      <Button text={"123"} type={"POSITIVE"} onClick={()=>{
+        console.log("123번 버튼 클릭");
+      }}/>
+      <Button text={"123"} type={"NEGATIVE"} onClick={()=>{
+        console.log("123번 버튼 클릭");
+      }}/>
+
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/new' element={<New />} />
